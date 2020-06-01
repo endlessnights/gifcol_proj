cec29261259bf03f35ea9832b7f49a47dcd829a3
Bakhti <me@byfe.me>
2020-06-01 18:15:04 +0300

    Добавил форму авторизации с условием/кнопку выхода

--- gifcol_app/urls.py
+++ gifcol_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.gifpage, name='gifpage'),
     path('video/', views.videopage, name='videopage'),
     path('img/', views.imgpage, name='imgpage'),
+    path('login/', views.a_login, name='a_login'),
+    path('logout/', views.a_logout, name='a_logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gifcol_app/views.py
+++ gifcol_app/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import authenticate
 from django.utils import timezone
-
-from django.shortcuts import render
+from django.contrib.auth import (
+    authenticate, login as built_in_login, logout as built_in_logout)
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+from django.views.decorators.http import require_POST
+
 from .models import mediamodel
 
 #Страница с Гифками
@@ -27,3 +32,21 @@ def imgpage(request):
                   {
                       'imgposts': imgposts
                   })
+
+# Login form Auth
+@require_POST
+def a_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        built_in_login(request, user)
+        messages.success(request, 'Авторизация пройдена успешно!')
+        return redirect('/')
+    else:
+        messages.warning(request, 'Введенные данные не верны!')
+        return redirect('/')
+# Logout form DeAuth
+def a_logout(request):
+    built_in_logout(request)
+    return redirect('/')
