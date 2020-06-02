53abaa0c79b9f094a07be23ef803df6fdc073b8e
Bakhti <me@byfe.me>
2020-06-02 15:52:57 +0300

    Поправил внешний вид

--- gifcol_app/views.py
+++ gifcol_app/views.py
@@ -47,10 +47,10 @@ def imgpage(request):
 # Профиль пользователя
 def get_user_profile(request, username):
     userprofile = User.objects.get(username=username)
-    files_added_by_user = mediamodel.objects.filter(author=userprofile, filetype='img')  # список файлов, добавленных пользователем
+    imgs_added_by_user = mediamodel.objects.filter(author=userprofile, filetype='img')  # список файлов, добавленных пользователем
     return render(request, 'app/user_profile.html', {
         "user": userprofile,
-        "files_added_by_user": files_added_by_user,
+        "imgs_added_by_user": imgs_added_by_user,
     })
 # Настройки профиля
 def edit_profile(request):
