9edf3a3832d17ded8b05ddcdaf1418438830cd96
Bakhti <me@byfe.me>
2020-06-13 14:01:53 +0300

    Правки ссылки для S3 Amazon

--- gifcol_proj/storage_backends.py
+++ gifcol_proj/storage_backends.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 # куда загружаем файлы на S3 Amazon
 class MediaStorage(S3Boto3Storage):
-    location = 'media/file'
+    location = 'media'
     file_overwrite = False
