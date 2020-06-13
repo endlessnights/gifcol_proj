fce993e59a320a295c2aae8625a1a1a13f755e8f
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2020-06-13 15:10:12 +0300

    Update settings.py

--- gifcol_proj/settings.py
+++ gifcol_proj/settings.py
@@ -62,9 +62,9 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#AWS_LOCATION = 'static'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 DEFAULT_FILE_STORAGE = 'gifcol_proj.storage_backends.MediaStorage'
@@ -149,5 +149,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, '')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '')
