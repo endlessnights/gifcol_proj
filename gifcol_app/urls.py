dfe347b83550e959bda690ea923284ba7999d95b
Евгений <evgeniy.levakov@raziogroup.com>
2020-06-13 12:39:06 +0300

    quick refactor

--- gifcol_app/urls.py
+++ gifcol_app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
 
-from .views import gifpage, new_mediafile, videopage, imgpage, logout, tag_link, bookmark_post
+from .views import gifpage, new_mediafile, videopage, imgpage, logout, TagLink, bookmark_post
 from accounts.views import register
 
 urlpatterns = [
@@ -15,5 +15,5 @@ urlpatterns = [
     path('img/', imgpage, name='imgpage'),
     url(r'(?P<id>\d+)/bookmark/$', bookmark_post, name='bookmark_post'),
     path('logout/', logout, name='logout'),
-    url(r'^tag/(?P<tag_link>[a-zA-Z0-9]+)$', tag_link.as_view(), name='tag_link'),
+    url(r'^tag/(?P<tag_link>[a-zA-Z0-9]+)$', TagLink.as_view(), name='tag_link'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
