e1a37734bf4e8b8f559ca01863c3e18af25b8d55
levakov <evgeniy.levakov@raziogroup.com>
2020-06-22 16:45:38 +0300

    pep8

--- gifcol_proj/settings.py
+++ gifcol_proj/settings.py
@@ -107,10 +107,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_gifcol_1',
-        'USER' : 'root',
-        'PASSWORD' : '=4JXuVJnzm',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'USER': 'root',
+        'PASSWORD': '=4JXuVJnzm',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
